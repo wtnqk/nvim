9fbd920c578109558a2cc2c4d734a56fbf1957c7
tecotec-watanabe-kei <watanabe.kei@tecotec.co.jp>
2024-11-12 13:11:54 +0900

    feat: 2024-11-12 :sparkles:

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "lsp" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
