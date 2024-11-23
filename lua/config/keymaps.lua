c06a403b04cbccdd536d692fd3a0546bd997329d
okgoglehisiri <okgoglehisiri@okgoglehisiris-Laptop.local>
2024-11-23 15:01:27 +0900

    feat: add split shortcut :+1:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+vim.keymap.set("n", "gd<Space>", ":split | lua vim.lsp.buf.definition()<CR>", bufopts)
+vim.keymap.set("n", "gd<CR>", ":vsplit | lua vim.lsp.buf.definition()<CR>", bufopts)
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -13,25 +13,28 @@ return {
       require("lspsaga").setup({
         finder = {
           max_height = 0.6,
+          -- これは必須です / REQUIRED
           default = "tyd+ref+imp+def",
+          -- ここは任意でお好きなキーバインドにしてください / optional
           keys = {
             toggle_or_open = "<CR>",
-            vsplit = { "v", "[" },
-            split = { "s", "]" },
+            vsplit = "v",
+            split = "s",
             tabnew = "t",
             tab = "T",
             quit = "q",
             close = "<Esc>",
           },
+          -- これは必須です / REQUIRED
           methods = {
             tyd = "textDocument/typeDefinition",
           },
         },
-        ui = {
-          code_action = "",
-        },
       })
 
+      -- ここで `leader` キーと `,` で、ポップアップで表示されます
+      -- 私の場合の `leader` キーは `,` なので、 `,,` で出てきてくれます
+      -- うれしい
       keymap.set("n", "<leader>,", "<Cmd>Lspsaga finder<CR>", { desc = "Telescope: live grep args" })
     end,
   },
