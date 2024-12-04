c96677e5f607a6c31ce180d9ba50c1ce8824acff
tecotec-watanabe-kei <watanabe.kei@tecotec.co.jp>
2024-12-04 22:38:35 +0900

    feat: blade対応 :+1:

---
+++ lua/plugins/php.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "css",
+        "c",
+        "c_sharp",
+        "cpp",
+        "php",
+        "blade",
+      },
+    },
+    config = function(_, opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
