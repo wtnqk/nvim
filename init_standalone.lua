6a00f546ae24cb8394c7b7d5b4ebbbffcf64fb8f
wtnqk <wtnqk@wtnqks-MacBook-Pro.local>
2025-06-11 06:52:38 +0900

    init更新

---
+++ init_standalone.lua
@@ -0,0 +1,17 @@
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("config.lazy_standalone")
+require("config.options_standalone")
+require("config.keymaps_standalone")
+require("config.autocmds_standalone")
