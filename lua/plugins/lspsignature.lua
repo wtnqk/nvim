0f3ad04a2ca973cd5f26fbf006134cfb2a1edabd
wtnqk <wtnqk@wtnqks-MacBook-Pro.local>
2025-07-23 22:20:20 +0900

    lspsignature remove

--- lua/plugins/lspsignature.lua
+++ lua/plugins/lspsignature.lua
@@ -1,13 +1,13 @@
 return {
   "ray-x/lsp_signature.nvim",
-  event = "InsertEnter",
-  opts = {
-    bind = true,
-    handler_opts = {
-      border = "rounded",
-    },
-  },
-  config = function(_, opts)
-    require("lsp_signature").setup(opts)
-  end,
+  -- event = "InsertEnter",
+  -- opts = {
+  --   bind = true,
+  --   handler_opts = {
+  --     border = "rounded",
+  --   },
+  -- },
+  -- config = function(_, opts)
+  --   require("lsp_signature").setup(opts)
+  -- end,
 }
